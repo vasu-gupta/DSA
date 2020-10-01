c0c712730cc6f05dd4c3757ff8ce283c6c1d4deb
Dhruvik-Chevli <43339792+Dhruvik-Chevli@users.noreply.github.com>
2020-10-01 13:26:54 +0530

    Let's use vectors instead

--- leetcode - climb stairs.cpp
+++ leetcode - climb stairs.cpp
@@ -5,17 +5,12 @@ public:
         if (n == 0 or n == 1 or n == 2)
             return n;
 
-        int dp[n + 1];
-        //No. of ways to get to 0th step
+        vector<ll>dp(n + 1,0);
         dp[0] = 0;
-        //No. of ways to get to 1st step (0->1)
         dp[1] = 1;
-        //No. of ways to get to 2nd step (0->1->2 or 0->2)
         dp[2] = 2;
-
         for (int i = 3 ; i <= n ; i++)
             dp[i] = dp[i - 1] + dp[i - 2];
-
         return dp[n];
     }
 };
